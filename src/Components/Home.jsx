436d36f62c3ac4a5353f4f91c36b75292de86ed0
Ayush Karan <ayushkaran328@gmail.com>
2025-02-07 22:21:47 +0530

    Update Home.jsx
    enhanced the styling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,31 +8,32 @@ const Home = () => {
 
   return (
     <div className="bg-gray-50 flex flex-col items-center justify-between min-h-screen">
-      <header className="w-full bg-black text-white p-4 flex justify-between items-center">
-        <h1 className="text-lg font-bold">FINANCE CARNIVAL</h1>
-        <div className="flex items-center space-x-4">
-          <button onClick={() => navigate('/User')}>
-            <div className="flex items-center space-x-4">
-              <div className="w-12 h-12 overflow-hidden bg-gray-200 rounded-full">
-                <img src={PROFILE} alt="User Profile" className="object-cover" />
-              </div>
-            </div>
-          </button>
-        </div>
+      {/* Header */}
+      <header className="w-full bg-black text-white p-3 flex justify-between items-center shadow-md">
+      <h1 className="font-semibold tracking-wide text-center w-full" style={{ fontSize: '30px' }}>Finance Carnival</h1>
+        <button onClick={() => navigate('/User')} className="focus:outline-none">
+          <div className="w-10 h-10 overflow-hidden bg-gray-200 rounded-full">
+            <img src={PROFILE} alt="User Profile" className="object-cover" />
+          </div>
+        </button>
       </header>
-      <main className="flex flex-col items-center space-y-4 mt-8">
-        <button onClick={() => navigate('/Scanner')} className="w-full py-4 px-6 rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300">
-          <span className="flex items-center justify-center">Make Payment</span>
+      
+      {/* Main Section */}
+      <main className="flex flex-col items-center space-y-5 mt-8 w-4/5">
+        <button onClick={() => navigate('/Scanner')} className="w-full py-4 px-6 text-lg font-medium rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300 shadow-md">
+          Make Payment
         </button>
-        <button onClick={() => navigate('/History')} className="w-full py-4 px-6 rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300">
-          <span className="flex items-center justify-center">Game History</span>
+        <button onClick={() => navigate('/History')} className="w-full py-4 px-6 text-lg font-medium rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300 shadow-md">
+          Game History
         </button>
-        <button onClick={() => navigate('/Money')} className="w-full py-4 px-6 rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300">
-          <span className="flex items-center justify-center">Add Coins to Wallet</span>
+        <button onClick={() => navigate('/Money')} className="w-full py-4 px-6 text-lg font-medium rounded-lg bg-gray-800 text-white hover:bg-gray-700 transition duration-300 shadow-md">
+          Add Coins to Wallet
         </button>
       </main>
+      
+      {/* Image Section */}
       <section className="w-4/5 mx-auto flex justify-center mt-8">
-        <img src={home} alt="Illustration" className="rounded-lg" />
+        <img src={home} alt="Illustration" className="rounded-lg w-full" />
       </section>
     </div>
   );
