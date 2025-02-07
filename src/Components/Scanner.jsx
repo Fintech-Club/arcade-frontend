1fe2079758098d10a54c6e4aea77ea0dd0115ff3
Ayush Karan <ayushkaran328@gmail.com>
2025-02-07 22:23:38 +0530

    Update Scanner.jsx
    Enhanced the styling

--- src/Components/Scanner.jsx
+++ src/Components/Scanner.jsx
@@ -5,12 +5,16 @@ const Scanner = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-white flex flex-col items-center justify-end h-screen pb-12">
-      <button onClick={() => navigate('/home')} className="absolute top-4 left-4 text-white bg-white text-2xl p-2 rounded-full">
+    <div className="bg-gray-50 flex flex-col items-center justify-center h-screen w-full relative">
+      {/* Back Button */}
+      <button onClick={() => navigate('/home')} 
+        className="absolute top-4 left-4 bg-black text-white text-2xl p-2 rounded-full">
         &#x2190;
       </button>
   
-      <button id="scanButton" className="bg-black text-white px-6 py-3 rounded-lg text-lg" onClick={() => navigate('/payment')}>
+      {/* Scan Button */}
+      <button id="scanButton" 
+        className="bg-black text-white px-6 py-3 rounded-lg text-lg fixed bottom-12" onClick={() => navigate('/payment')}>
         Scan
       </button>
     </div>
